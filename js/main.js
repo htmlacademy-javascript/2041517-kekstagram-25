5ff08009ebfe6a656afc1ac56a7bc0692dd0216b
Max Dolgovskii <punkratej@gmail.com>
2022-03-14 14:36:51 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
+let randomNumber = function (a, b) {
+   if (a >= 0 & a <= b) {
+    Math.random (a, b);
+   } else {
+       return "Ошибка"
+   }
 
+};
 
-
-
-let commentLength = function (a, b) {
-    if (a <= b & a >= 0) {
+let commentLength = function (a) {
+    if (a <= 140 & a >= 0) {
         return true;
 
 
+    } else {
+        return false;
     }
     
   
 };
 
 commentLength(3, 140);
-
+randomNumber(6, 56);
