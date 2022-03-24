4fcf3dbf72443249a4300592fd24b0326b11a3a7
Max Dolgovskii <punkratej@gmail.com>
2022-03-24 23:41:18 +0300

    Модуляция

--- js/data.js
+++ js/data.js
@@ -1,93 +1,86 @@
-import {getRandomInt, getRandomArrayElement} from 'util.js';
-
+import {getRandomInt, getRandomArrayElement} from './util.js';
 
 const DESCRIPTIONS = [
-    'Красота',
-    'Это я увидел в отпуске',
-    'Аж сердце замерло',
-    'Аппетитный вид',
-    'Прикольно, да?:)',
-  ];
+  'Красота',
+  'Это я увидел в отпуске',
+  'Аж сердце замерло',
+  'Аппетитный вид',
+  'Прикольно, да?:)',
+];
 
-const MESSAGES = [
- 'Всё отлично!',
- 'В целом всё неплохо. Но не всё.',
- 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
- 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
- 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
- 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+ const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
  ];
 
-const NAMES = [
-    'Сергей',
-    'Максим',
-    'Нина',
-    'Иван',
-    'Дмитрий',
-    'Мария',
-    'Анна',
-    'Станислав',
-    'Виталия',
-    'Алексей',
-    'Родион',
-    'Алла',
-    'Екатерина',
-    'Надежда',
-    'Капитолина',
-    'Марина'
- ];
-
+ const NAMES = [
+  'Сергей',
+  'Максим',
+  'Нина',
+  'Иван',
+  'Дмитрий',
+  'Мария',
+  'Анна',
+  'Станислав',
+  'Виталия',
+  'Алексей',
+  'Родион',
+  'Алла',
+  'Екатерина',
+  'Надежда',
+  'Капитолина',
+  'Марина'
+];
 const MAX_COMMENTS_COUNT = 15;
 const photosCount = 25;
 
-function generateId () {
-    let arrayId = [];    
-    for (let i = 0; i < 25; i++) {
-      let random = getRandomInt(1, 25);  
-      if (arrayId.indexOf(random)) {
-          return;
-      } else {
-      arrayId.push(random);
-      };
+const generateRandomId = function () {
+  let arrayId = [];
+  for (let i = 0; i < photosCount; ++i) {
+    let randomId = getRandomInt(1, 500);
+    if (arrayId.indexOf(randomId) === -1) {
+    arrayId.push(randomId);
     };
-    return getRandomArrayElement(arrayId);
+  };  
+  return getRandomArrayElement(arrayId);
 };
 
-function generateAvatar () {
-    return 'img/avatar-' + getRandomInt(1, 6) + '.svg';
+let introId = 0;
+const generateId = function () {
+  return ++introId;
 };
 
-function generatePhotoUrl () {
-    let arrayId = [];    
-    for (let i = 0; i < 25; i++) {
-      let random = getRandomInt(1, 25); 
-      if (arrayId.indexOf(random)) {
-          return;
-      } else {
-      arrayId.push(random);
-        };
-       };           
-  return 'photos/' + getRandomArrayElement(arrayId) + '.jpg,';
+const generateAvatar = function () {
+  return 'img/avatar-' + getRandomInt(1, 6) + '.svg';
 };
+let introPhotoUrl = 0;
+const generatePhotoUrl = function () {
+   return 'photos/' + ++introPhotoUrl + '.jpg,';
+  };
 
-const createComment = function() {
-    return {
-      id: generateId(),
-      avatar: generateAvatar(),
-      message: getRandomArrayElement(MESSAGES),
-      name: getRandomArrayElement(NAMES),
-    };
-};
-
+const createComment = () => {
+  return {
+    id: generateRandomId(),
+    avatar: generateAvatar(),
+    message: getRandomArrayElement(MESSAGES),
+    name: getRandomArrayElement(NAMES),
+  };
+ };
+  
 const createObject = () => {
-    return {
-      id: generateId(),
-      url: generatePhotoUrl(),
-      description: getRandomArrayElement(DESCRIPTIONS),
-      likes: getRandomInt(15, 200),
-      comments: Array.from({length: getRandomInt(0, MAX_COMMENTS_COUNT)}, createComment),
-    };
+  return {
+    id: generateId(),
+    url: generatePhotoUrl(),
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomInt(15, 200),
+    comments: Array.from({length: getRandomInt(0, MAX_COMMENTS_COUNT)}, createComment),
+  };
 };
 
-export {createObject};
-export {createComment};
+const getPhotos = Array.from({length: photosCount}, createObject);
+
+export {getPhotos};
