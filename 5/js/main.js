e889727ef717b9840c1ae308874075c90b555a5c
Keks <keks@htmlacademy.ru>
2022-03-24 19:31:40 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/main.js
+++ 5/js/main.js
@@ -1,38 +1,38 @@
 const DESCRIPTIONS = [
-    'Красота',
-    'Это я увидел в отпуске',
-    'Аж сердце замерло',
-    'Аппетитный вид',
-    'Прикольно, да?:)',
-  ];
+  'Красота',
+  'Это я увидел в отпуске',
+  'Аж сердце замерло',
+  'Аппетитный вид',
+  'Прикольно, да?:)',
+];
 
  const MESSAGES = [
- 'Всё отлично!',
- 'В целом всё неплохо. Но не всё.',
- 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
- 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
- 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
- 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
  ];
 
  const NAMES = [
-    'Сергей',
-    'Максим',
-    'Нина',
-    'Иван',
-    'Дмитрий',
-    'Мария',
-    'Анна',
-    'Станислав',
-    'Виталия',
-    'Алексей',
-    'Родион',
-    'Алла',
-    'Екатерина',
-    'Надежда',
-    'Капитолина',
-    'Марина'
- ];
+  'Сергей',
+  'Максим',
+  'Нина',
+  'Иван',
+  'Дмитрий',
+  'Мария',
+  'Анна',
+  'Станислав',
+  'Виталия',
+  'Алексей',
+  'Родион',
+  'Алла',
+  'Екатерина',
+  'Надежда',
+  'Капитолина',
+  'Марина'
+];
 const MAX_COMMENTS_COUNT = 15;
 const photosCount = 25;
 /*
@@ -48,67 +48,57 @@ const getRandomNumber = function (min, max) {
 */
 
 const getRandomInt = (min, max) => {
-  if (min < 0 || max < 0) {return - 1; };
-  if (min > max) { [min, max] = [max, min]};
+  if (min < 0 || max < 0) return - 1;
+  if (min > max) [min, max] = [max, min];
   return Math.floor((Math.random() * (max - min + 1) + min));
 };
 
-
 const getRandomArrayElement = (elements) => {
-    return elements[getRandomInt(0, elements.length - 1)];
-  };
+  return elements[getRandomInt(0, elements.length - 1)];
+};
 
-
-
-
-const generateId = function () {
+const generateRandomId = function () {
   let arrayId = [];
-  for (let i = 0; i < 25; ++i) {
-    let random = getRandomInt(1, 25);
-    if (arrayId.indexOf(random) === -1) {
-    arrayId.push(random);
+  for (let i = 0; i < photosCount; ++i) {
+    let randomId = getRandomInt(1, 500);
+    if (arrayId.indexOf(randomId) === -1) {
+    arrayId.push(randomId);
     };
-  };
+  };  
   return getRandomArrayElement(arrayId);
+};
+
+let introId = 0;
+const generateId = function () {
+  return ++introId;
 };
 
 const generateAvatar = function () {
   return 'img/avatar-' + getRandomInt(1, 6) + '.svg';
 };
-
-
+let introPhotoUrl = 0;
 const generatePhotoUrl = function () {
-    let arrayId = [];    
-    for (let i = 0; i < 25; ++i) {
-      let randomNumber = getRandomInt(1, 25);
-      if (arrayId.indexOf(randomNumber) === -1) {
-      arrayId.push(randomNumber);
-       };
-      };     
-  return 'photos/' + getRandomArrayElement(arrayId) + '.jpg,';
+   return 'photos/' + ++introPhotoUrl + '.jpg,';
+  };
+
+const createComment = () => {
+  return {
+    id: generateRandomId(),
+    avatar: generateAvatar(),
+    message: getRandomArrayElement(MESSAGES),
+    name: getRandomArrayElement(NAMES),
+  };
 };
-  
-  
-  const createComment = function() {
-    return {
-      id: generateId(),
-      avatar: generateAvatar(),
-      message: getRandomArrayElement(MESSAGES),
-      name: getRandomArrayElement(NAMES),
-    };
+
+const createObject = () => {
+  return {
+    id: generateId(),
+    url: generatePhotoUrl(),
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomInt(15, 200),
+    comments: Array.from({length: getRandomInt(0, MAX_COMMENTS_COUNT)}, createComment),
   };
-
-
-
- const createObject = () => {
-    return {
-      id: generateId(),
-      url: generatePhotoUrl(),
-      description: getRandomArrayElement(DESCRIPTIONS),
-      likes: getRandomInt(15, 200),
-      comments: Array.from({length: getRandomInt(0, MAX_COMMENTS_COUNT)}, createComment),
-    };
-  };
+};
 
 const photos = Array.from({length: photosCount}, createObject);
 
