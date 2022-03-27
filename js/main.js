76b201a368035973a43b377320730e9d35f33f3d
Max Dolgovskii <punkratej@gmail.com>
2022-03-27 16:10:45 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -106,3 +106,31 @@ const photos = Array.from({length: photosCount}, createObject);
 console.log(photos);
 
 
+
+
+let getRandomNumber = function (min, max) {
+  if (min >= 0 & min <= max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else {
+      return 'Ошибка'
+  }
+
+};
+
+let checkCommentLength = function (string, length) {
+  
+   if (string <= length & string >= 0) {
+       return true;
+
+
+   } else {
+       return false;
+   }
+   
+ 
+};
+
+checkCommentLength(3, 140);
+getRandomNumber(6, 56);
