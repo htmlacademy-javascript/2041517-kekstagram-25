82fad834651dcec48d1c8319489bbe6276b2c53d
Max Dolgovskii <punkratej@gmail.com>
2022-04-12 20:14:32 +0300

    Update photos.js

--- js/photos.js
+++ js/photos.js
@@ -2,18 +2,15 @@ import {generatePhotos} from './data.js';
 const photoContainer = document.querySelector('.pictures')
 
 const similarPhotos = generatePhotos();
-const templateFragment = document.querySelector('#picture').content;
-
-const template = templateFragment.querySelector('a');
-
+const templateFragment = document.querySelector('#picture').content.querySelector('.picture');
 
 const similarListFragment = document.createDocumentFragment();
 
 similarPhotos.forEach(({url, likes, comments}) => {
-  const photosElement = similarWizardTemplate.cloneNode(true);
+  const photosElement = templateFragment.cloneNode(true);
   photosElement.querySelector('.picture__img').src = url;
   photosElement.querySelector('.picture__likes').textContent = likes;
-  photosElement.querySelector('.picture__comments').textContent = comments;
+  photosElement.querySelector('.picture__comments').textContent = comments.length;
   similarListFragment.appendChild(photosElement);
 });
 
